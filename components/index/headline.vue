a030a9b9b43c16debe8a0a15cfb41c2ce760fd00
Sindy Stefani <sindystefani7@gmail.com>
2019-07-25 11:27:30 +0700

    app image

--- components/index/headline.vue
+++ components/index/headline.vue
@@ -1,15 +1,10 @@
 <template>
     <section>
       <div v-for="(item, index) in field" :key="index" class="dflex align--items__center">
-        <div class="headline--image glitch"
-            :style="`background-image: url(${item.image})`">
-            <div class="glitch__img"></div>
-            <div class="glitch__img"></div>
-            <div class="glitch__img"></div>
-            <div class="glitch__img"></div>
-            <div class="glitch__img"></div>
-            <img :src="item.image" alt="Article Title">
-        </div>  
+        <appImage 
+          :urlImage="item.image"
+          class="headline--image"
+        />
         <div class="headline--content">
             <nuxt-link to="detail" class="headline--content__category">{{item.category}}</nuxt-link>
             <nuxt-link to="detail" class="headline--content__title"><h2>{{item.title}}</h2></nuxt-link>
@@ -25,12 +20,16 @@
 
 <script>
 import { articles } from '../../assets/dummy/articles'
+import appImage from '~/components/widget/appImage.vue'
 export default {
   data () {
     return {
       articles,
       field: []
     }
+  },
+  components: {
+    appImage
   },
   mounted(){
       this.field = articles.slice(0,1)
---
+++ components/widget/appImage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="glitch" :style="`background-image: url(${urlImage})`">
+        <div class="glitch__img"></div>
+        <div class="glitch__img"></div>
+        <div class="glitch__img"></div>
+        <div class="glitch__img"></div>
+        <div class="glitch__img"></div>
+        <img :src="urlImage" alt="Article Title">
+     </div> 
+</template>
+
+<script>
+export default {
+    props: {
+        urlImage: String
+    }
+}
+</script>
